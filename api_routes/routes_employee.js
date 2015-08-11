d7bce7ac2fbee06c3377692aaa53bf2b68c54c92
Nicolas Bordenave <nicolas.bordenave@autodesk.com>
2015-08-11 12:29:39 -0300

    add restriction. for routes_employee you must specify what you want as response. json/xml

--- api_routes/routes_employee.js
+++ api_routes/routes_employee.js
@@ -1,27 +1,66 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
+var js2xmlparser = require("js2xmlparser");
 
-//var mongoose = require('mongoose');
 var employee = require('../models/employee.js');
 
 
 
 /* GET /employee listing. */
 router.get('/', function(req, res, next) {
-    console.log("routes_employee. getEmployeeList()")
+    console.log("routes_employee. getEmployeeList()");
+
     employee.find(function (err, employee) {
         if (err) return next(err);
-        res.json(employee);
+        if(req.accepts('*/*')){
+            res.send('You have to specify what you accept from the server', 400);
+        }else if(req.accepts('xml')){
+            // This function is failling.
+            res.type('xml');
+            var options = {
+                wrapArray: {
+                    enabled: true,
+                    elementName : 'Employee'
+                }
+            };
+            var employeeList = res.json(employee);
+            res.send(js2xmlparser("Employees", employeeList, options)); //we need the wrapArray options here to generate valid xml structure
+        }else if(req.accepts('json')){
+            res.json(employee); //res.send(JSON.stringify(employees)) works as well
+        }else{
+            res.send('Accept format not allowed', 406);
+        }
+
+
+        //res.json(employee);
     });
 });
 
 /* GET /todos/id */
 router.get('/:id', function(req, res, next) {
+    console.log("Geting employee : ",req.params.id);
+
     employee.findOne({'id' : req.params.id}, function (err, employee) {
-        console.log("Geting employee : ",req.params.id);
-        if (err) return next(err);
-        res.json(employee);
+
+        if(req.accepts('*/*')){
+            res.send('You have to specify what you accept from the server', 400);
+        }else if(req.accepts('xml')){
+            // This function is failling.
+            res.type('xml');
+            var options = {
+                wrapArray: {
+                    enabled: true,
+                    elementName : 'Employee'
+                }
+            };
+            var employeeList = res.json(employee);
+            res.send(js2xmlparser("Employees", employeeList, options)); //we need the wrapArray options here to generate valid xml structure
+        }else if(req.accepts('json')){
+            res.json(employee); //res.send(JSON.stringify(employees)) works as well
+        }else{
+            res.send('Accept format not allowed', 406);
+        }
     });
 
 });
